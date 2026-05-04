24594480aa2aacea8171dfdf0a9287d7724c9939
Israel-igbafe <igbafe.israel@lmu.edu.ng>
2026-05-04 17:06:09 +0100

    Update sendReminder.js

--- api/sendReminder.js
+++ api/sendReminder.js
@@ -15,7 +15,7 @@ const REMINDER_EMAIL_FROM =
 
 const REMINDER_SUBJECT =
   process.env.REMINDER_EMAIL_SUBJECT ||
-  "Congratulations! Your Scholarship award 🎓;
+  "Congratulations! Your Scholarship award 🎓";
 
 const DEFAULT_CUTOFF_HOURS = Number(process.env.REMINDER_CUTOFF_HOURS || 24);
 const MINIMUM_CUTOFF_HOURS = 24;
